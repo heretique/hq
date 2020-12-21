be0481490a224d4d1494683d114cc592520e0042
Catalin Moldovan <moldovan.catalin@gmail.com>
2020-12-21 19:42:45 +0200

    Added reedbeta's enumerate.

---
+++ include/Hq/Enumerate.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <tuple>
+
+// Implementation from http://reedbeta.com/blog/python-like-enumerate-in-cpp17/
+
+namespace hq
+{
+
+template <typename T,
+          typename TIter = decltype(std::begin(std::declval<T>())),
+          typename = decltype(std::end(std::declval<T>()))>
+constexpr auto enumerate(T && iterable)
+{
+    struct iterator
+    {
+        size_t i;
+        TIter iter;
+        bool operator != (const iterator & other) const { return iter != other.iter; }
+        void operator ++ () { ++i; ++iter; }
+        auto operator * () const { return std::tie(i, *iter); }
+    };
+    struct iterable_wrapper
+    {
+        T iterable;
+        auto begin() { return iterator{ 0, std::begin(iterable) }; }
+        auto end() { return iterator{ 0, std::end(iterable) }; }
+    };
+    return iterable_wrapper{ std::forward<T>(iterable) };
+}
+
+} // hq namespace
