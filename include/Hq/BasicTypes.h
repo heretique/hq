6a2b3ee904a779dec3f0ffee74f405906310336f
Catalin Moldovan <moldovan.catalin@gmail.com>
2021-01-25 19:07:34 +0200

    Prep for Shared lib.

--- include/Hq/BasicTypes.h
+++ include/Hq/BasicTypes.h
@@ -22,9 +22,7 @@ using uptr = uintptr_t;
 
 // https://foonathan.net/2020/09/move-forward/
 // Move replacement for std::move, like Type obj = MOVE(other_obj);
-#define MOVE(...) \
-static_cast<std::remove_reference_t<decltype(__VA_ARGS__)>&&>(__VA_ARGS__)
+#define MOVE(...) static_cast<std::remove_reference_t<decltype(__VA_ARGS__)>&&>(__VA_ARGS__)
 
 // Forward, use like  fn(FWD(args)...);
-#define FWD(...) \
-static_cast<decltype(__VA_ARGS__) &&>(__VA_ARGS__)
+#define FWD(...) static_cast<decltype(__VA_ARGS__) &&>(__VA_ARGS__)
